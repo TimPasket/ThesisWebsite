c82ce7ddd35e63c66c37692adf8f7ef8b0a71f32
Tim Pasket <tim.d.pasket@gmail.com>
2022-04-15 19:07:00 -0400

    More backend setup

--- thesisDjango/users/models.py
+++ thesisDjango/users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class userAccounts(models.Model):
-  name = models.CharField(max_length=200, null=True)
+  userNname = models.CharField(max_length=200, null=True)
   email = models.CharField(max_length=200, null=True)
   dateCreated = models.DateTimeField(auto_now_add=True)
+  role = models.CharField(max_length=200, default='subscriber')
--- thesisDjango/users/views.py
+++ thesisDjango/users/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from .models import *
-from .forms import OrderForm
-from .filters import OrderFilter
 
 def registerPage(request):
   form = UserCreationForm()
   context = {'form'}
   return render(request, 'Frontend/register.html', context)
 def loginPage(request):
-  context = {}
+  context = {'form'}
   return render(request, 'Frontend/login.html', context)
