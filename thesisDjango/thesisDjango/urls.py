20b5139f38a9988bf91cd2b4add486a50de8e16a
Tim Pasket <tim.d.pasket@gmail.com>
2022-05-09 00:34:28 -0400

    permissions added/restricted to edit other users post

--- thesisDjango/thesisDjango/urls.py
+++ thesisDjango/thesisDjango/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('users', include('django.contrib.auth.urls'))
 ]
--- thesisDjango/users/views.py
+++ thesisDjango/users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.urls import reverse_lazy
 
